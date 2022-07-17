0e7d7196da1751c2c447e3021ae29792b0fd86ba
A1 Inventalk <A1@inventalkcom.onmicrosoft.com>
2022-07-17 20:30:46 +0800

    資料庫

--- Models/Products.cs
+++ Models/Products.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -12,11 +13,15 @@ namespace MVC.Models
         public int SupplierID { get; set; }
         public int CategoryID { get; set; }
         public string QuantityPerUnit { get; set; }
-        public int UnitPrice { get; set; }
+        public decimal UnitPrice { get; set; }
         public int UnitsInStock { get; set; }
         public int UnitsOnOrder { get; set; }
         public int ReorderLevel { get; set; }
-        public int Discontinued { get; set; }
+        public byte Discontinued { get; set; }
 
+    }
+    public class DataDBContext : DbContext
+    {
+        public DbSet<Products> Products { get;set;}
     }
 }
